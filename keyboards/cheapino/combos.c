72f0a734f43fd9c516da4346ab1bf3ea9e892e92
shan <shandoor@gmail.com>
2026-03-27 14:50:17 +0100

    Removed combos

--- keyboards/cheapino/combos.c
+++ keyboards/cheapino/combos.c
@@ -1,12 +1,12 @@
-#include "quantum.h"
-
-const uint16_t PROGMEM combo0[] = {KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM combo1[] = {KC_U, KC_I, COMBO_END};
-const uint16_t PROGMEM combo2[] = {MT(MOD_LGUI, KC_D), MT(MOD_LSFT, KC_F), COMBO_END};
-const uint16_t PROGMEM combo3[] = {MT(MOD_RSFT, KC_J), MT(MOD_RGUI, KC_K), COMBO_END};
-const uint16_t PROGMEM combo4[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM combo5[] = {KC_M, KC_COMMA, COMBO_END};
-
-combo_t key_combos[] = {
-    COMBO(combo0, KC_LBRC), COMBO(combo1, KC_RBRC), COMBO(combo2, KC_LPRN), COMBO(combo3, KC_RPRN), COMBO(combo4, KC_LCBR), COMBO(combo5, KC_RCBR),
-};
+// #include "quantum.h"
+//
+// const uint16_t PROGMEM combo0[] = {KC_E, KC_R, COMBO_END};
+// const uint16_t PROGMEM combo1[] = {KC_U, KC_I, COMBO_END};
+// const uint16_t PROGMEM combo2[] = {MT(MOD_LGUI, KC_D), MT(MOD_LSFT, KC_F), COMBO_END};
+// const uint16_t PROGMEM combo3[] = {MT(MOD_RSFT, KC_J), MT(MOD_RGUI, KC_K), COMBO_END};
+// const uint16_t PROGMEM combo4[] = {KC_C, KC_V, COMBO_END};
+// const uint16_t PROGMEM combo5[] = {KC_M, KC_COMMA, COMBO_END};
+//
+// combo_t key_combos[] = {
+//     COMBO(combo0, KC_LBRC), COMBO(combo1, KC_RBRC), COMBO(combo2, KC_LPRN), COMBO(combo3, KC_RPRN), COMBO(combo4, KC_LCBR), COMBO(combo5, KC_RCBR),
+// };
