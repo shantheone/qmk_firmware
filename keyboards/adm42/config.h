fe6256873bb1442ab09bba2f373d8d9a5b05d087
shantheone <shandoor@gmail.com>
2026-07-22 14:38:41 +0200

    Added adm42 custom config, based on miryoku

---
+++ keyboards/adm42/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Custom config
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define TAPPING_TERM 250
+#define PERMISSIVE_HOLD
+#define FLOW_TAP_TERM 150
+#define CHORDAL_HOLD
+#define SPECULATIVE_HOLD
+#define SELECT_WORD_OS_MAC
